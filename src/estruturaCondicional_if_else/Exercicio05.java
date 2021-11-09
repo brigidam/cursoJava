339cd18c0923b7476b66493383a99a55d04205a6
Brigida Montenegro <brigida.montenegrogualberto@gmail.com>
2021-11-09 17:33:40 -0300

    Estrutura Condicional Exercicio 05

---
+++ src/estruturaCondicional_if_else/Exercicio05.java
@@ -0,0 +1,36 @@
+package estruturaCondicional_if_else;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio05 {
+
+    public static void main(String[]args){
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int cod = sc.nextInt();
+        int quant = sc.nextInt();
+
+        double total;
+        if (cod == 1){
+            total = quant * 4.0;
+        }
+        else if (cod == 2){
+            total = quant * 4.5;
+        }
+        else if (cod == 3){
+            total = quant * 5.0;
+        }
+        else if (cod == 4){
+            total = quant * 2.0;
+        }
+        else {
+            total = quant * 1.5;
+        }
+        System.out.printf("Total: R$ %.2f%n", total);
+
+        sc.close();
+    }
+}
