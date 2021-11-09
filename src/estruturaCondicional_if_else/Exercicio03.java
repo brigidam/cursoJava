b3139184f21b72a10515e97b3289c53e7758affa
Brigida Montenegro <brigida.montenegrogualberto@gmail.com>
2021-11-09 17:28:19 -0300

    Estrutura Condicional Exercicio 03

---
+++ src/estruturaCondicional_if_else/Exercicio03.java
@@ -0,0 +1,22 @@
+package estruturaCondicional_if_else;
+
+import java.util.Scanner;
+
+public class Exercicio03 {
+
+    public static void main(String[]args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a % b == 0 || b % a == 0){
+            System.out.println("São Múltiplos");
+        }else {
+            System.out.println("Não são Múltiplos");
+        }
+
+        sc.close();
+    }
+}
