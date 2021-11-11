1e33894e4122c7662925ec7c7321d054853fa7f6
Brigida Montenegro <brigida.montenegrogualberto@gmail.com>
2021-11-10 23:12:19 -0300

    Estrutura Repetitiva For

--- src/estruturaRepetitiva_for/Exercicio04.java
+++ src/estruturaRepetitiva_for/Exercicio04.java
@@ -7,7 +7,7 @@ public class Exercicio04 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        
+
         int n = sc.nextInt();
 
         for (int i=0; i<n; i++) {
