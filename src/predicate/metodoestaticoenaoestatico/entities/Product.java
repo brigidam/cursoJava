a3c9ad707ef09f8a81626fd3800be86c67da88fd
Brigida Montenegro <brigida.montenegrogualberto@gmail.com>
2022-01-31 21:18:10 -0300

    Predicate (exemplo com removeIf) utilizando a implementação da interface, reference method com método estático, com método não estático, expressão lambda decladara e expressão lambda inline

---
+++ src/predicate/metodoestaticoenaoestatico/entities/Product.java
@@ -0,0 +1,43 @@
+package predicate.metodoestaticoenaoestatico.entities;
+
+public class Product {
+
+    private String name;
+    private Double price;
+
+    public Product(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public static boolean staticProductPredicate(Product p) { // Reference method com método estático
+        return p.getPrice() >= 100.0;
+    }
+
+    /*public boolean nonStaticProductPredicate() { // Reference method com método não estático
+        return price() >= 100.0;
+    }*/
+
+    @Override
+    public String toString() {
+        return  name
+                + ", "
+                + String.format("%.2f", price);
+    }
+}
